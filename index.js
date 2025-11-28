1afc577a0ec5a0099c7ea1e24fe4ff3ac3f43d41
Aksinister <91866119+Aksinister@users.noreply.github.com>
2025-11-28 14:43:45 +1000

    I fixed my Javascript
    I defined the input correctly so that the AI was sent the correct information

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function printWorkout(response) {
 }
 function generateWorkout(event) {
   event.preventDefault();
-
-  let prompt = `Please generate a list of no more than 4 calisthenics exercises from this prompt:${input} . You must check whether or not the exercises you choose are related to the prompt.Display the name of each exercise like this: <strong>Squats</strong>: 4 sets of 12 reps.Please make sure to include a <br /> between each line too. Do not anounce anything conversational, simply print eh exercise`;
+  let userInput = document.querySelector("#input");
+  let prompt = `Please generate a list of no more than 7 calisthenics exercises from this prompt:${userInput.value}.Display the name of each exercise like this: <strong>Squats</strong>: 4 sets of 12 reps.Please make sure to include a <br /> between each line too. Do not anounce anything conversational, simply print eh exercise`;
   let context =
     "You make lists of calisthenics workouts.You only suggest exercises that have a focus on the body part that you are given.";
   let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
