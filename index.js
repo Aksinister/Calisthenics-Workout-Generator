d24f5a850551b2a7dc8080119fe62223950cc0bc
Aksinister <91866119+Aksinister@users.noreply.github.com>
2025-11-28 12:18:25 +1000

    Added base JS functionality
    Added an event listener and a typewriter plug-in for added styling when the API response is displayed
    
    I also polished up the CSS on the submit button, as it did not match the colour of the input field when hovered,

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//prompt("Hello World!");
+function printWorkout(event) {
+  event.preventDefault();
+
+  new Typewriter("#printed-workout", {
+    strings: ["hello world"],
+    autoStart: true,
+    delay: 50,
+  });
+}
+
+let submitForm = document.querySelector("#inputForm");
+submitForm.addEventListener("submit", printWorkout);
