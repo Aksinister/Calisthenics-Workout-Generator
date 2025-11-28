bb2c0a64a916d134760f350f501110dd563556f5
Aksinister <91866119+Aksinister@users.noreply.github.com>
2025-11-28 15:43:15 +1000

    Polished up the UX features
    Created a "generating workout" message with a blinking animation for the emoji

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function printWorkout(response) {
-  new Typewriter("#printed-workout", {
+  new Typewriter("#workout", {
     strings: [response.data.answer],
     autoStart: true,
     delay: 50,
@@ -8,11 +8,15 @@ function printWorkout(response) {
 function generateWorkout(event) {
   event.preventDefault();
   let userInput = document.querySelector("#input");
-  let prompt = `Please generate a list of no more than 7 calisthenics exercises from this prompt:${userInput.value}.Display the name of each exercise like this: <strong>Squats</strong>: 4 sets of 12 reps.Please make sure to include a <br /> between each line too. Do not anounce anything conversational, simply print eh exercise`;
+  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
   let context =
     "You make lists of calisthenics workouts.You only suggest exercises that have a focus on the body part that you are given.";
-  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
+  let prompt = `Please generate a list of no more than 7 calisthenics exercises from this prompt:${userInput.value}.Display the name of each exercise like this: <strong>Squats</strong>: 4 sets of 12 reps.Please make sure to include a <br /> between each line too. Do not anounce anything conversational, simply print the exercise`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let workout = document.querySelector("#workout");
+  workout.classList.remove("hide");
+  workout.innerHTML = `<span class = "blink">🤔</span> Generating a workout for you that targets the  ${userInput.value} ...`;
 
   axios.get(apiUrl).then(printWorkout);
 }
